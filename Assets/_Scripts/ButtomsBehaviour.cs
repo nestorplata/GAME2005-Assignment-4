1ad4fd0fb72ca3ec6e31354b135d2a8bd348aba2
nestorplata <Nestor.PlataGomez@georgebrown.ca>
2020-12-14 18:45:11 -0500

    start scene completed

--- Assets/_Scripts/ButtomsBehaviour.cs
+++ Assets/_Scripts/ButtomsBehaviour.cs
@@ -1,18 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ButtomsBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void PlayGame ()
     {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void QuitGame ()
     {
-        
+        Debug.Log("QUIT!");
+        Application.Quit();
     }
+
+    public void StartScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
 }
+
