226718cd04dfa2327d6daa357e7985a9aa668c9d
Kian <kianbadiei@gmail.com>
2020-12-18 16:11:53 -0500

    Bullet Manager Implemented

--- Assets/_Scripts/BulletBehaviour2.cs
+++ Assets/_Scripts/BulletBehaviour2.cs
@@ -6,21 +6,33 @@ using UnityEngine;
 
 public class BulletBehaviour2 : MonoBehaviour
 {
-
     public float speed;
     public Vector3 direction;
+    public float range;
+    public float radius;
+    public bool debug;
+    public bool isColliding;
+    public Vector3 collisionNormal;
+    public float penetration;
 
+    public BulletManager bulletManager;
 
     // Start is called before the first frame update
     void Start()
     {
-        speed = 4;
-       
+        isColliding = false;
+        radius = Mathf.Max(transform.localScale.x, transform.localScale.y, transform.localScale.z) * 0.5f;
+        bulletManager = FindObjectOfType<BulletManager>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position += (-direction) * speed * Time.deltaTime;
+        if (Vector3.Distance(transform.position, Vector3.zero) > range)
+        {
+            bulletManager.ReturnBullet(this.gameObject);
+        }
     }
 }
